8721660c99cc12d64617030e12f0b6ce82ebc997
Mahesh Waran <mahesh99waran@gmail.com>
2021-01-12 23:16:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.querySelector("#id1").style.color="white";
+    document.querySelector("#id1").style.backgroundColor="darkblue";
+    document.querySelector("#id1").style.width="110%";
+    document.querySelector("#id1").style.height="90px";
+    document.querySelector("#id1").style.paddingTop ="40px";
+    document.querySelector("#id1").innerHTML="Mahesh Waran";
+    document.querySelector("#id1").style.border="5px solid white";
+    document.body.style.backgroundImage="url('https://st4.depositphotos.com/10325396/21636/i/1600/depositphotos_216360648-stock-photo-digital-code-number-abstract-background.jpg')";
+}
